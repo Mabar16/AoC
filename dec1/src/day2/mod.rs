d4b59f1604e03fd4c83e9fc39a270f4685e6b15b
Markus Barrow <stone1996@live.dk>
2023-12-09 15:37:51 +0100

    Day 2 part 2

--- dec1/src/day2/mod.rs
+++ dec1/src/day2/mod.rs
@@ -14,12 +14,17 @@ pub fn run(){
     let red_pattern = Regex::new(r"(?<red_count>\d{1,3})(?= red)").unwrap();
     let blue_pattern = Regex::new(r"(?<blue_count>\d{1,3})(?= blue)").unwrap();
     let green_pattern = Regex::new(r"(?<green_count>\d{1,3})(?= green)").unwrap();
-
+    
     let mut sum = 0;
+    let mut power = 0;
     // get game id, compare red/blue/green to max for each round (split on ;)
     for game in games{
         let hands: Vec<String> = game.split(';').map(str::to_string).collect();
         let mut valid = true;
+
+        let mut min_red = 0;
+        let mut min_blue = 0;
+        let mut min_green = 0;
 
         for hand in hands{
 
@@ -27,31 +32,40 @@ pub fn run(){
             if red_count.is_some(){
                 let red_cubes_nr = red_count.unwrap().as_str().to_owned().parse::<i32>().unwrap();
                 valid = valid && (red_cubes_nr <= red_cubes);
+                if red_cubes_nr > min_red{
+                    min_red = red_cubes_nr;
+                }
             }
 
             let green_count = green_pattern.find(&hand).unwrap();
             if green_count.is_some(){
                 let green_cubes_nr = green_count.unwrap().as_str().to_owned().parse::<i32>().unwrap();
                 valid = valid && (green_cubes_nr <= green_cubes);
+                if green_cubes_nr > min_green{
+                    min_green = green_cubes_nr;
+                }
             }
 
             let blue_count = blue_pattern.find(&hand).unwrap();
             if blue_count.is_some(){
                 let blue_cubes_nr = blue_count.unwrap().as_str().to_owned().parse::<i32>().unwrap();
                 valid = valid && (blue_cubes_nr <= blue_cubes);
+                if blue_cubes_nr > min_blue{
+                    min_blue = blue_cubes_nr;
+                }
             }
         }
 
         let game_id = game_pattern.find(&game).unwrap();
         
         if valid {
-            println!("Valid: {}", &game);
             sum += game_id.unwrap().as_str().to_owned().parse::<i32>().unwrap();  
-        } else {
-            println!("Inalid: {}", &game);
         }
+
+        power += min_blue * min_green * min_red;
     }
     
-    println!("{}", &sum);
+    println!("Sum: {}", &sum);
+    println!("Power: {}", &power);
 
 }
